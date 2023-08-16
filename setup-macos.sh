822eb49eb78eb5b61c2a73fa5bb46b3f2ee30a1c
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2023-08-16 23:21:43 +0200

    Meta: Update macOS configuration.

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # ========== URLs ==========
 URL_BREW='https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'
 
@@ -14,6 +15,17 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCorner
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+defaults write com.apple.dock show-recents -bool NO # Disable recent apps
+killall Dock 2> /dev/null
+
+defaults write com.apple.finder AppleShowAllFiles -bool YES # Show all files
+defaults write com.apple.finder ShowPathbar -bool YES # Show the path bar
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool NO # Disable the extension change warning
+killall Finder 2> /dev/null
+
+defaults write -g AppleICUForce24HourTime -bool NO # Use 12-hour time
+defaults write -g com.apple.sound.beep.feedback -bool YES # Make noise when using the volume keys
 echo 'OK'
 
 # ========== Homebrew ==========
